7c0bb3604e3bffdfc77fdf115d8807cbd6ac6637
Laeti2312 <38733102+Laeti2312@users.noreply.github.com>
2018-05-02 20:22:40 +0200

    Update Run.js

--- Run.js
+++ Run.js
@@ -20,7 +20,7 @@ var Baltos;
 var Balto = ["Balto","Jenna","Aleu","Aniu","Kodi","Steele","Niju","Saba","Dingo","Rosy","Boris","Star","Dixie","Sylvie","Nava","Dusty","Stella","The Red-faced pup","Kaltag","The Grizzly Bear","Balto's Father","Ralph","Kirby","Muk","Luk","Nikki","The Red-blaze pup","Nuk","Yak","Sumac","Grandma Rosy","Vike","Mel","Dipsy","Duke","Steele's Musher","Other Bull Moose","Bull Moose","Totem animals",];
 
 const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
+
 
 bot.login(process.env.TOKEN);
 
@@ -92,17 +92,18 @@ bot.on('message', function (message){
         if (message.content.toLowerCase().startsWith('r!evil')){
             
             var reason = message.content.substring(6, message.content.length);
-            message.channel.send(  reason + ' is ' +  Math.floor(random(100,0)) + '% evil'); 
+            message.channel.send(  reason + ' is ' +  Math.floor(random(100,0)) + '% evil');
+		 }
 		
-	if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the args back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
+	if(message.content.toLowerCase().startsWith('r!say')) {
+	    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+	    // To get the "message" itself we join the args back into a string with spaces: 
+	    const sayMessage = args.join(" ");
+	    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+	    message.delete().catch(O_o=>{}); 
+	    // And we get the bot to say the thing: 
+	    message.channel.send(sayMessage);
+	 }
             
 	}
 		
