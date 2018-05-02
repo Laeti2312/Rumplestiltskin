0caa54f5d1610ec4623b7a74ef1d73faa2315ffd
Laeti2312 <38733102+Laeti2312@users.noreply.github.com>
2018-05-02 20:18:24 +0200

    Update Run.js

--- Run.js
+++ Run.js
@@ -94,7 +94,7 @@ bot.on('message', function (message){
             var reason = message.content.substring(6, message.content.length);
             message.channel.send(  reason + ' is ' +  Math.floor(random(100,0)) + '% evil'); 
 		
-	if(command === "r!say") {
+	if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the args back into a string with spaces: 
     const sayMessage = args.join(" ");
